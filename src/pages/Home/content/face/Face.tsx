9d552aac4de803cdf550748bd22b2db3acba5ab4
MaximKemmel <52887742+MaximKemmel@users.noreply.github.com>
2024-02-05 16:55:56 +0200

    Face

--- src/pages/Home/content/face/Face.tsx
+++ src/pages/Home/content/face/Face.tsx
@@ -1,5 +1,4 @@
-import { useRef, useState } from "react";
-import Slider from "react-slick";
+import { useLayoutEffect, useState } from "react";
 import parse from "html-react-parser";
 
 import styles from "./Face.module.sass";
@@ -10,26 +9,37 @@ import { IProductionCategory } from "../../../../types/production/productionCate
 
 const Face = () => {
   const [currentProductionCategory, setCurrentProductionCategory] = useState(productionCategoriesList[0]);
-  const slider = useRef(null as Slider);
+  const [isTransition, setIsTransition] = useState(false);
 
-  const settings = {
-    className: "center",
-    infinite: true,
-    centerPadding: "50px",
-    centerMode: true,
-    vertical: true,
-    slidesToShow: 3,
-    swipeToSlide: false,
-    autoplay: false,
-    arrows: false,
-    variableWidth: true,
-  };
+  useLayoutEffect(() => {
+    setCurrentProductionCategory(productionCategoriesList[0]);
+    var carousel = document.getElementById("carousel");
+    var items = document.getElementsByClassName("carousel_item");
+    carousel?.scrollTo({
+      top: (items[0] as HTMLElement).offsetTop - 400,
+      behavior: "smooth",
+    });
+  }, []);
 
-  const handleProductionOnClick = (productionCategory: IProductionCategory) => {
-    setCurrentProductionCategory(productionCategory);
+  const handleProductionOnClick = async (productionCategory: IProductionCategory) => {
+    setIsTransition(true);
+    var carousel = document.getElementById("carousel");
+    carousel!.style.removeProperty("transform");
+    var items = document.getElementsByClassName("carousel_item");
+    var sum = 750;
+    for (let index = 0; index < productionCategory.id; index++) {
+      sum += items[index].clientHeight;
+    }
+    sum = items[productionCategory.id].clientHeight;
+    carousel?.scrollTo({
+      top: (items[productionCategory.id] as HTMLElement).offsetTop - 500,
+      behavior: "smooth",
+    });
     var indicator = document.getElementById("indicator");
     indicator!.style.marginLeft = `${20 + productionCategory.id * 80 + productionCategory.id * 30}px`;
-    slider?.current?.slickGoTo(productionCategory.id);
+    await new Promise((res) => setTimeout(res, 200));
+    setCurrentProductionCategory(productionCategory);
+    setIsTransition(false);
   };
 
   return (
@@ -50,22 +60,43 @@ const Face = () => {
             </div>
             <div className={styles.description_container}>
               <div className={styles.description}>
-                <div className={styles.name}>{currentProductionCategory.name}</div>
-                <div className={styles.about}>{parse(currentProductionCategory.description)}</div>
-                <div className={styles.bottom}></div>
+                <div className={styles.text}>
+                  <div className={`${styles.name} ${!isTransition ? styles.active : ""}`}>
+                    {currentProductionCategory.full_name}
+                  </div>
+                  <div className={`${styles.about} ${!isTransition ? styles.active : ""}`}>
+                    {parse(currentProductionCategory.description)}
+                  </div>
+                  <div className={styles.bottom}>
+                    <div
+                      className={`${styles.price} ${!isTransition ? styles.active : ""}`}
+                    >{`от ${currentProductionCategory.min_price.toLocaleString()}₽`}</div>
+                    <button type="button">Перейти в каталог</button>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
-          <div className={styles.slider}>
-            <Slider ref={slider} {...settings}>
+          <div className={styles.slider} id="carousel">
+            <div className={styles.carousel}>
+              <img
+                className={"carousel_item_tmp"}
+                src={`/uploads/production/${productionCategoriesList[productionCategoriesList.length - 1].prefix}.png`}
+                alt=""
+              />
               {productionCategoriesList.map((productionCategory: IProductionCategory) => (
                 <img
+                  className={`carousel_item ${currentProductionCategory.id === productionCategory.id ? styles.active : ""}`}
                   src={`/uploads/production/${productionCategory.prefix}.png`}
-                  className={currentProductionCategory.id === productionCategory.id ? styles.active : ""}
                   alt=""
                 />
               ))}
-            </Slider>
+              <img
+                className={"carousel_item_tmp"}
+                src={`/uploads/production/${productionCategoriesList[0].prefix}.png`}
+                alt=""
+              />
+            </div>
           </div>
         </div>
       </div>
